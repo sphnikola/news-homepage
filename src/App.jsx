e94ee872993481ff6c78f4e3eb4b3b0324c90059
sphnikola <sphnikola@gmail.com>
2023-12-02 22:14:02 +0000

    fix content in center without part of it cutting away

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Footer } from "./Footer";
 function App() {
   return (
     <>
-      <div className="max-w-[70rem] mx-auto font-inter h-screen grid place-content-center">
+      <div className="max-w-[70rem] mx-auto font-inter grid place-content-center min-h-screen">
         <Body />
         <Footer />
       </div>
