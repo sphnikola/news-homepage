50f85ed02461885999a9df04e688da54af8d8b46
sphnikola <sphnikola@gmail.com>
2023-12-02 21:53:38 +0000

    add the whole content to the center of the page using grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import Body from "./Body";
+import { Footer } from "./Footer";
+
 function App() {
   return (
     <>
-      <div className="max-w-[70rem] mx-auto">
+      <div className="max-w-[70rem] mx-auto font-inter h-screen grid place-content-center">
         <Body />
+        <Footer />
       </div>
     </>
   );
