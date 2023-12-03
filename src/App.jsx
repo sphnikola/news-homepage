0d315fbbc8073303bd0a2ae872e73ca29ecc73f3
sphnikola <sphnikola@gmail.com>
2023-12-03 18:49:44 +0000

    add navabr component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Body from "./Body";
 import { Footer } from "./Footer";
+import { Navbar } from "./Navbar";
 
 function App() {
   return (
     <>
       <div className="max-w-[70rem] mx-auto font-inter grid place-content-center min-h-screen">
+        <Navbar />
         <Body />
         <Footer />
       </div>
