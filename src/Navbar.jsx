54a5bfe13172ed7cb40da47df863d1d2e16144c1
sphnikola <sphnikola@gmail.com>
2023-12-03 21:13:06 +0000

    add navbar for large screens without working hamburger menu on small screens

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,67 +1,12 @@
 import logo from "./assets/images/logo.svg";
 import ham from "./assets/images/icon-menu.svg";
-import { useState } from "react";
 
 export function Navbar() {
-  const [click, SetClick] = useState(false);
-  if (click) {
-    const page = (
-      <div className="min-h-screen w-full bg-white">
-        <ul className="flex flex-col gap-y-5  text-light-gray">
-          <li>
-            <a
-              className=" hover:text-light-pink focus:text-light-pink"
-              href="#"
-            >
-              Home
-            </a>
-          </li>
-          <li>
-            <a
-              className=" hover:text-light-pink focus:text-light-pink"
-              href="#"
-            >
-              New
-            </a>
-          </li>
-          <li>
-            <a
-              className=" hover:text-light-pink focus:text-light-pink"
-              href="#"
-            >
-              Popular
-            </a>
-          </li>
-          <li>
-            <a
-              className=" hover:text-light-pink focus:text-light-pink"
-              href="#"
-            >
-              Trending
-            </a>
-          </li>
-          <li>
-            <a
-              className=" hover:text-light-pink focus:text-light-pink"
-              href="#"
-            >
-              Categories
-            </a>
-          </li>
-        </ul>
-      </div>
-    );
-    return page;
-  }
-  const handleClick = () => {
-    SetClick(true);
-  };
-
   return (
     <div className="mx-4 py-8">
       <div className="flex justify-between items-center">
         <img className="" src={logo} />
-        <button onClick={handleClick} className="lg:hidden">
+        <button className="lg:hidden">
           <img src={ham} />
         </button>
         <div className="hidden lg:block">
