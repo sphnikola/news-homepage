5bded0ec36f3fa7639b3c8d031ffb51c7aa8182b
sphnikola <sphnikola@gmail.com>
2024-01-11 14:01:03 +0000

    Add Responsive Navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,83 +1,161 @@
+// import { useState } from "react";
+// // import { close, logo, menu } from "../assets";
+// import close from "./assets/images/icon-menu-close.svg";
+// import menu from "./assets/images/icon-menu.svg";
+
+// export const navLinks = [
+//   {
+//     id: "home",
+//     title: "Home",
+//   },
+//   {
+//     id: "features",
+//     title: "Features",
+//   },
+//   {
+//     id: "product",
+//     title: "Product",
+//   },
+//   {
+//     id: "clients",
+//     title: "Clients",
+//   },
+// ];
+
+// const Navbar = () => {
+//   const [active, setActive] = useState("Home");
+//   const [toggle, setToggle] = useState(false);
+
+//   return (
+//     <nav className=" w-full flex py-6 justify-between items-center navbar">
+//       {/* Logo */}
+//       <h1 className="text-3xl text-white">Logo</h1>
+
+//       {/* Desktop Navigation */}
+//       <ul className="list-none sm:flex hidden justify-end items-center flex-1">
+//         {navLinks.map((nav, index) => (
+//           <li
+//             key={nav.id}
+//             className={`font-poppins font-normal cursor-pointer text-[16px] ${
+//               active === nav.title ? "text-white" : "text-dimWhite"
+//             } ${index === navLinks.length - 1 ? "mr-0" : "mr-10"}`}
+//             onClick={() => setActive(nav.title)}
+//           >
+//             <a href={`#${nav.id}`}>{nav.title}</a>
+//           </li>
+//         ))}
+//       </ul>
+
+//       {/* Mobile Navigation */}
+//       <div className="sm:hidden flex flex-1 justify-end items-center">
+//         <img
+//           src={toggle ? close : menu}
+//           alt="menu"
+//           className="w-[28px] h-[28px] object-contain"
+//           onClick={() => setToggle(!toggle)}
+//         />
+
+//         {/* Sidebar */}
+//         <div
+//           className={`${
+//             !toggle ? "hidden" : "flex "
+//           } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2  min-w-[140px] rounded-xl sidebar`}
+//         >
+//           <ul className="list-none flex justify-end items-start flex-1 flex-col ">
+//             {navLinks.map((nav, index) => (
+//               <li
+//                 key={nav.id}
+//                 className={`font-poppins font-medium cursor-pointer text-[16px] ${
+//                   active === nav.title ? "text-white" : "text-dimWhite"
+//                 } ${index === navLinks.length - 1 ? "mb-0" : "mb-4"}`}
+//                 onClick={() => setActive(nav.title)}
+//               >
+//                 <a href={`#${nav.id}`}>{nav.title}</a>
+//               </li>
+//             ))}
+//           </ul>
+//         </div>
+//       </div>
+//     </nav>
+//   );
+// };
+
+// export default Navbar;
+
+// Navbar.js
+// for nextjs 13.4 users
 import { useState } from "react";
-// import { close, logo, menu } from "../assets";
+import open from "./assets/images/icon-menu.svg";
 import close from "./assets/images/icon-menu-close.svg";
-import menu from "./assets/images/icon-menu.svg";
-
-export const navLinks = [
-  {
-    id: "home",
-    title: "Home",
-  },
-  {
-    id: "features",
-    title: "Features",
-  },
-  {
-    id: "product",
-    title: "Product",
-  },
-  {
-    id: "clients",
-    title: "Clients",
-  },
-];
-
 const Navbar = () => {
-  const [active, setActive] = useState("Home");
-  const [toggle, setToggle] = useState(false);
-
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const navLinks = [
+    { href: "#home", label: "Home" },
+    { href: "#about-us", label: "About Us" },
+    { href: "#products", label: "Products" },
+    { href: "#contact-us", label: "Contact Us" },
+  ];
   return (
-    <nav className=" w-full flex py-6 justify-between items-center navbar">
-      {/* Logo */}
-      <h1 className="text-3xl text-white">Logo</h1>
-
-      {/* Desktop Navigation */}
-      <ul className="list-none sm:flex hidden justify-end items-center flex-1">
-        {navLinks.map((nav, index) => (
-          <li
-            key={nav.id}
-            className={`font-poppins font-normal cursor-pointer text-[16px] ${
-              active === nav.title ? "text-white" : "text-dimWhite"
-            } ${index === navLinks.length - 1 ? "mr-0" : "mr-10"}`}
-            onClick={() => setActive(nav.title)}
-          >
-            <a href={`#${nav.id}`}>{nav.title}</a>
-          </li>
-        ))}
-      </ul>
-
-      {/* Mobile Navigation */}
-      <div className="sm:hidden flex flex-1 justify-end items-center">
-        <img
-          src={toggle ? close : menu}
-          alt="menu"
-          className="w-[28px] h-[28px] object-contain"
-          onClick={() => setToggle(!toggle)}
-        />
-
-        {/* Sidebar */}
-        <div
-          className={`${
-            !toggle ? "hidden" : "flex"
-          } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2  min-w-[140px] rounded-xl sidebar`}
-        >
-          <ul className=" list-none flex justify-end items-start flex-1 flex-col mt-0">
-            {navLinks.map((nav, index) => (
-              <li
-                key={nav.id}
-                className={`font-poppins font-medium cursor-pointer text-[16px] ${
-                  active === nav.title ? "text-white" : "text-dimWhite"
-                } ${index === navLinks.length - 1 ? "mb-0" : "mb-4"}`}
-                onClick={() => setActive(nav.title)}
-              >
-                <a href={`#${nav.id}`}>{nav.title}</a>
+    <>
+      <header className="sm:px-8 px-4 py-2 z-10 w-full">
+        <nav className="flex justify-between items-center max-container">
+          <a href="/" className="text-3xl font-bold">
+            Logo
+          </a>
+          <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
+            {navLinks.map((item) => (
+              <li key={item.label}>
+                <a
+                  href={item.href}
+                  className="font-montserrat leading-normal text-lg text-slate-gray"
+                >
+                  {item.label}
+                </a>
               </li>
             ))}
           </ul>
+          <div className="flex gap-2 text-lg leading-normal font-medium font-montserrat max-lg:hidden wide:mr-24">
+            <a href="/">Sign in</a>
+            <span>/</span>
+            <a href="/">Explore now</a>
+          </div>
+          <div
+            className="hidden max-lg:block cursor-pointer"
+            onClick={() => {
+              setIsMenuOpen(!isMenuOpen);
+            }}
+          >
+            <img src={open} className="text-4xl" />
+          </div>
+        </nav>
+      </header>
+      {isMenuOpen && (
+        <div>
+          <nav className="fixed top-0 right-0 left-0 bottom-0 lg:bottom-auto bg-slate-100  ">
+            <div
+              className="hidden max-lg:block fixed right-0  px-8 py-4 cursor-pointer"
+              onClick={() => {
+                setIsMenuOpen(!isMenuOpen);
+              }}
+            >
+              <img src={close} className="text-4xl" />
+            </div>
+            <ul className=" lg:hidden flex flex-col items-center justify-center h-full ">
+              {navLinks.map((item) => (
+                <li key={item.label}>
+                  <a
+                    href={item.href}
+                    className="font-montserrat leading-normal text-lg text-slate-gray"
+                  >
+                    {item.label}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </nav>
         </div>
-      </div>
-    </nav>
+      )}
+    </>
   );
 };
-
 export default Navbar;
